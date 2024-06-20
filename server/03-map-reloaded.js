c6a8201f0a576d0f4abba8edbda53395fbe58bbe
AdrianGerman <adrian.german1019@gmail.com>
2024-06-20 11:53:16 -0600

    map reloaded

---
+++ server/03-map-reloaded.js
@@ -0,0 +1,44 @@
+const orders = [
+  {
+    customerName: "Adrian",
+    total: 60,
+    delivered: true
+  },
+  {
+    customerName: "Julieta",
+    total: 120,
+    delivered: false
+  },
+  {
+    customerName: "Moctezuma",
+    total: 180,
+    delivered: true
+  },
+  {
+    customerName: "Valentina",
+    total: 240,
+    delivered: true
+  }
+];
+
+console.log("original", orders);
+const rta = orders.map((item) => item.total);
+console.log("rta", rta);
+
+// const rta2 = orders.map((item) => {
+//   item.tax = 0.19;
+//   return item;
+// });
+
+// console.log("rta2", rta2);
+// console.log("original", orders);
+
+const rta3 = orders.map((item) => {
+  return {
+    ...item,
+    tax: 0.19
+  };
+});
+
+console.log("rta3", rta3);
+console.log("original", orders);
